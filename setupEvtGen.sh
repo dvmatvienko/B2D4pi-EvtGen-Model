c482da274cf74e0a0781ae44edc1b5de077e3155
Andrii Verbytskyi <andriish@hermes.mpp.mpg.de>
2020-03-09 22:16:08 +0100

    Another attempt: docs and script

--- setupEvtGen.sh
+++ setupEvtGen.sh
@@ -12,15 +12,22 @@
 # https://phab.hepforge.org/source/evtgen/browse/master/setupEvtGen.sh?view=raw
 
 # Version or tag number. No extra spaces on this line!
-VERSION=cmake
+VERSION=smallerissuesav2
 # Pythia version number with no decimal points, e.g. 8230 corresponds to version 8.230. This
 # follows the naming convention of Pythia install tar files. Again, no extra spaces allowed
-PYTHIAVER=8230
+PYTHIAVER=8243
 PYTHIAPKG="pythia"$PYTHIAVER
 PYTHIATAR=$PYTHIAPKG".tgz"
 echo Pythia version set to $PYTHIAVER, package tar name $PYTHIATAR
 
 HEPMCMAJORVERSION="3"
+
+#This is for systems with cmake and cmake3
+if command -v cmake3; then
+CMAKE=cmake3
+else
+CMAKE=cmake
+fi
 
 mkdir -p EvtGen
 cd EvtGen
@@ -56,8 +63,8 @@ echo Extracting external dependencies
 tar -xzf hepmc2.06.10.tgz
 tar -xzf HepMC3-3.2.0.tar.gz
 tar -xzf $PYTHIATAR
-tar -xzf PHOTOS.3.61.tar.gz
-tar -xzf TAUOLA.1.1.6c.tar.gz
+tar -xzf PHOTOS.3.64.tar.gz
+tar -xzf TAUOLA.1.1.8.tar.gz
 
 # Patch TAUOLA and PHOTOS on Darwin (Mac)
 if [ "$osArch" == "Darwin" ]
@@ -67,70 +74,76 @@ then
 fi
 
 
-
-
-
-
-
-
 if [ "$HEPMCMAJORVERSION" -lt "3" ]
 then
 echo Installing HepMC in $INSTALL_BASE/external/HepMC
 mkdir -p HepMC
 mkdir -p HepMC.build
 cd HepMC.build
-cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_BASE/external/HepMC $INSTALL_BASE/external/hepmc2.06.10 -Dmomentum:STRING=GEV -Dlength:STRING=MM
+$CMAKE -DCMAKE_INSTALL_PREFIX=$INSTALL_BASE/external/HepMC $INSTALL_BASE/external/HepMC-2.06.10 -Dmomentum:STRING=GEV -Dlength:STRING=MM
 make
 make install
 
 echo Installing pythia8 in $INSTALL_BASE/external/$PYTHIAPKG
 cd ../$PYTHIAPKG
-if [ "$PYTHIAVER" -lt "8200" ]
+if [  "$PYTHIAVER" -lt "8300"  ]
 then
-  ./configure --with-hepmc=$INSTALL_BASE/external/HepMC --with-hepmcversion=2.06.10 --enable-shared
-else
-  ./configure --with-hepmc2=$INSTALL_BASE/external/HepMC --enable-shared
+  ./configure --enable-shared --prefix=$INSTALL_BASE/external/$PYTHIAPKG
 fi
+if [ "$PYTHIAVER" -ge "8300" ]
+then
+  ./configure --enable-shared --prefix=$INSTALL_BASE/external/$PYTHIAPKG
+fi
+
 make
+make install
 
 echo Installing PHOTOS in $INSTALL_BASE/external/PHOTOS
 cd ../PHOTOS
-./configure --with-hepmc=$INSTALL_BASE/external/HepMC
+./configure --with-hepmc3= --with-hepmc=$INSTALL_BASE/external/HepMC --prefix=$INSTALL_BASE/external/PHOTOS
 make
+make install
 
 echo Installing TAUOLA in $INSTALL_BASE/external/TAUOLA
 cd ../TAUOLA
-./configure --with-hepmc=$INSTALL_BASE/external/HepMC
+./configure --without-hepmc3 --with-hepmc=$INSTALL_BASE/external/HepMC  --prefix=$INSTALL_BASE/external/TAUOLA
 make
+make install
+
 else
 
 echo Installing HepMC3 in $INSTALL_BASE/external/HepMC3
 mkdir -p HepMC3
 mkdir -p HepMC3.build
 cd HepMC3.build
-cmake  -DHEPMC3_ENABLE_ROOTIO=OFF -DHEPMC3_ENABLE_PYTHON=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_BASE/external/HepMC3 $INSTALL_BASE/external/HepMC3-3.2.0
+$CMAKE  -DHEPMC3_ENABLE_ROOTIO=OFF -DHEPMC3_ENABLE_PYTHON=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_BASE/external/HepMC3 $INSTALL_BASE/external/HepMC3-3.2.0
 make
 make install
 
 echo Installing pythia8 in $INSTALL_BASE/external/$PYTHIAPKG
 cd ../$PYTHIAPKG
-if [ "$PYTHIAVER" -lt "8200" ]
+if [ "$PYTHIAVER" -lt "8300"  ]
 then
-  ./configure --enable-shared
-else
-  ./configure --with-hepmc3=$INSTALL_BASE/external/HepMC3 --enable-shared
+  ./configure --enable-shared --prefix=$INSTALL_BASE/external/$PYTHIAPKG
 fi
+if [ "$PYTHIAVER" -ge "8300" ]
+then
+  ./configure --prefix=$INSTALL_BASE/external/$PYTHIAPKG
+fi
+
 make
 
 echo Installing PHOTOS in $INSTALL_BASE/external/PHOTOS
 cd ../PHOTOS 
-./configure --with-hepmc3=$INSTALL_BASE/external/HepMC3
+./configure --with-hepmc3=$INSTALL_BASE/external/HepMC3 --prefix=$INSTALL_BASE/external/PHOTOS
 make
+make install 
 
 echo Installing TAUOLA in $INSTALL_BASE/external/TAUOLA
 cd ../TAUOLA
-./configure  --with-hepmc3=$INSTALL_BASE/external/HepMC3
+./configure  --with-hepmc3=$INSTALL_BASE/external/HepMC3 --prefix=$INSTALL_BASE/external/TAUOLA
 make
+make install 
 fi
 
 echo Building EvtGen
@@ -140,12 +153,13 @@ mkdir -p evtgen
 cd evtgen.build
 if [ "$HEPMCMAJORVERSION" -lt "3" ]
 then
-cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_BASE/evtgen $INSTALL_BASE/evtgen.git -DEVTGEN_PYTHIA=ON -DEVTGEN_PHOTOS=ON -DEVTGEN_TAUOLA=ON -DEVTGEN_HEPMC3=OFF
+$CMAKE -DCMAKE_INSTALL_PREFIX=$INSTALL_BASE/evtgen $INSTALL_BASE/evtgen.git -DEVTGEN_BUILD_TESTS=ON -DEVTGEN_PYTHIA=ON -DEVTGEN_PHOTOS=ON -DEVTGEN_TAUOLA=ON -DEVTGEN_HEPMC3=OFF 
 else
-cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_BASE/evtgen $INSTALL_BASE/evtgen.git -DEVTGEN_PYTHIA=ON -DEVTGEN_PHOTOS=ON -DEVTGEN_TAUOLA=ON
+$CMAKE -DCMAKE_INSTALL_PREFIX=$INSTALL_BASE/evtgen $INSTALL_BASE/evtgen.git -DEVTGEN_BUILD_TESTS=ON -DEVTGEN_PYTHIA=ON -DEVTGEN_PHOTOS=ON -DEVTGEN_TAUOLA=ON -DEVTGEN_HEPMC3=ON -DHepMC3_DIR=$INSTALL_BASE/external/HepMC3/share/HepMC3/cmake/
 fi
 make
 make install
+ctest .
 cd $INSTALL_BASE/evtgen
 
 echo Setup done.
