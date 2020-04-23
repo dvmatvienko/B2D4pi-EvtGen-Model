4492c9568c0e42bb0c510010979d5afdac47ad8b
Thomas Latham <T.Latham@warwick.ac.uk>
2020-04-23 14:56:20 +0100

    Apply copyright and licence notices to all relevant files

--- validation/plotBKstarGamma.C
+++ validation/plotBKstarGamma.C
@@ -1,3 +1,23 @@
+
+/***********************************************************************
+* Copyright 1998-2020 CERN for the benefit of the EvtGen authors       *
+*                                                                      *
+* This file is part of EvtGen.                                         *
+*                                                                      *
+* EvtGen is free software: you can redistribute it and/or modify       *
+* it under the terms of the GNU General Public License as published by *
+* the Free Software Foundation, either version 3 of the License, or    *
+* (at your option) any later version.                                  *
+*                                                                      *
+* EvtGen is distributed in the hope that it will be useful,            *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+* GNU General Public License for more details.                         *
+*                                                                      *
+* You should have received a copy of the GNU General Public License    *
+* along with EvtGen.  If not, see <https://www.gnu.org/licenses/>.     *
+***********************************************************************/
+
 // Macro to create validation plots for testing PHOTOS for
 // B0 -> K*0' gamma, K*0' -> K+ pi- nGamma, where n = 0,1,2,...
 // Uses the ROOT files created by genRootDecayChain program
